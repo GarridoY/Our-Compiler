7b7a3ffcd240b9482e6c3facf9909473ea0836be
Simon Mathiasen <smath17@student.aau.dk>
2020-10-09 12:45:50 +0200

    removed cast stream to tokens to stream
    too many methods

--- tests/dk/aau/cs/d703e20/astTest.java
+++ tests/dk/aau/cs/d703e20/astTest.java
@@ -9,8 +9,6 @@ import dk.aau.cs.d703e20.parser.OurParser;
 import dk.aau.cs.d703e20.resources.FailTestErrorListener;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
-import org.antlr.v4.runtime.ListTokenSource;
-import org.antlr.v4.runtime.Token;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -19,34 +17,23 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class astTest {
 
-    // Turn string into list of lexer tokens
-    private List<Token> getTokensFromText(String txt) {
+    private OurParser createParserFromText(String txt) {
         // Instantiate lexer from input text
         OurLexer lexer = new OurLexer(CharStreams.fromString(txt));
         // Add ANTLR errorListener to throw syntaxError exceptions, reports offending symbol and line
         lexer.addErrorListener(new FailTestErrorListener());
         CommonTokenStream tokenStream = new CommonTokenStream(lexer);
-        tokenStream.fill();
-        return tokenStream.getTokens();
-    }
 
-    private OurParser createParser(List<Token> tokens) {
-        ListTokenSource tokenSource = new ListTokenSource(tokens);
-        CommonTokenStream cts = new CommonTokenStream(tokenSource);
-        OurParser parser = new OurParser(cts);
+        // Instantiate parser from lexer tokens
+        OurParser parser = new OurParser(tokenStream);
         parser.addErrorListener(new FailTestErrorListener()); // Not expecting any syntax error, fails test if any
         return parser;
-    }
-
-    private  OurParser createParser(String txt) {
-        List<Token> tokens = getTokensFromText(txt);
-        return createParser(tokens);
     }
 
     // Test AST for conditional statement if
     @Test
     void testIF() {
-        OurParser parser = createParser("if (hej == true) {}");
+        OurParser parser = createParserFromText("if (hej == true) {}");
         OurParser.IfStatementContext ifStatement = parser.ifStatement();
         ASTBuilder astBuilder = new ASTBuilder();
         IfStatementNode ifStatementNode = (IfStatementNode) astBuilder.visitIfStatement(ifStatement);
@@ -66,7 +53,7 @@ public class astTest {
     // Test AST for conditional statement if else if
     @Test
     void testIFELSEIF() {
-        OurParser parser = createParser("if (hej == true) {} else if (test) {} else {}");
+        OurParser parser = createParserFromText("if (hej == true) {} else if (test) {} else {}");
         OurParser.IfElseStatementContext ifElseStatement = parser.ifElseStatement();
         ASTBuilder astBuilder = new ASTBuilder();
         IfElseStatementNode ifElseStatementNode = (IfElseStatementNode) astBuilder.visitIfElseStatement(ifElseStatement);
@@ -86,7 +73,7 @@ public class astTest {
     // Test AST for bool expression
     @Test
     void testBOOLEXPR() {
-        OurParser parser = createParser("test1 == test2;");
+        OurParser parser = createParserFromText("test1 == test2;");
         OurParser.BoolExprContext boolExpr = parser.boolExpr();
         ASTBuilder astBuilder = new ASTBuilder();
         BoolExpressionNode boolExpressionNode = (BoolExpressionNode) astBuilder.visitBoolExpr(boolExpr);
