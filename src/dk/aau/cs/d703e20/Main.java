1ad7415f01bd6a322793cafa647809744cd06ec1
HTML_Earth <htmlearth@gmail.com>
2020-11-02 11:17:25 +0100

    Preparing for UPPAAL stuff

--- src/dk/aau/cs/d703e20/Main.java
+++ src/dk/aau/cs/d703e20/Main.java
@@ -6,6 +6,7 @@ import dk.aau.cs.d703e20.ast.structure.ProgramNode;
 import dk.aau.cs.d703e20.parser.OurLexer;
 import dk.aau.cs.d703e20.parser.OurParser;
 import dk.aau.cs.d703e20.semantics.SemanticChecker;
+import dk.aau.cs.d703e20.semantics.TimeVerifier;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import java.io.IOException;
@@ -13,10 +14,16 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) {
         String inputFileName = null;
+        boolean prettyPrint = false;
 
         if (args.length > 0) {
             // Set file name which is to be compiled.
             inputFileName = args[0];
+
+            if (args.length > 1) {
+                if (args[1].equals("-pp"))
+                    prettyPrint = true;
+            }
         }
 
         if (inputFileName == null) {
@@ -41,15 +48,21 @@ public class Main {
             ProgramNode programNode = (ProgramNode) astBuilder.visitProgram(parseTree);
             System.out.println("AST ok");
 
+            //PRETTY PRINT
+            if (prettyPrint) {
+                System.out.println("Pretty print:\n");
+                System.out.println(programNode.prettyPrint(0));
+            }
+
             // SEMANTICS
             SemanticChecker semanticChecker = new SemanticChecker();
             semanticChecker.visitProgram(programNode);
             System.out.println("Semantics ok");
 
-            //PRETTY PRINT
-            //TODO: make optional
-            System.out.println("Pretty print:\n");
-            System.out.println(programNode.prettyPrint(0));
+            // VERIFY TIME IN UPPAAL
+            TimeVerifier timeVerifier = new TimeVerifier();
+            timeVerifier.verifyProgram(programNode);
+            System.out.println("Time ok");
         }
         catch (IOException e) {
             e.printStackTrace();
---
+++ src/dk/aau/cs/d703e20/semantics/TimeVerifier.java
@@ -0,0 +1,13 @@
+package dk.aau.cs.d703e20.semantics;
+
+import dk.aau.cs.d703e20.ast.structure.ProgramNode;
+
+public class TimeVerifier {
+    public TimeVerifier() {
+
+    }
+
+    public void verifyProgram(ProgramNode programNode) {
+
+    }
+}
