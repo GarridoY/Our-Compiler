c6151c0dc6d86447c780ba271a85cdb628504c9d
Rynke <37619846+Rynke@users.noreply.github.com>
2020-11-03 13:04:35 +0100

    Create ArduinoGenerator.java

---
+++ src/dk/aau/cs/d703e20/codegen/ArduinoGenerator.java
@@ -0,0 +1,10 @@
+package dk.aau.cs.d703e20.codegen;
+
+import dk.aau.cs.d703e20.ast.errorhandling.CompilerException;
+import dk.aau.cs.d703e20.ast.Enums;
+import dk.aau.cs.d703e20.ast.expressions.*;
+import dk.aau.cs.d703e20.ast.statements.*;
+
+
+public class ArduinoGenerator {
+}
