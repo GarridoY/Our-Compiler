a8c5174245d577c7cf1f38688b129e05d4ccbcc6
Rynke <37619846+Rynke@users.noreply.github.com>
2020-11-03 15:36:53 +0100

    Update ArduinoGenerator.java
    + visitProgram

--- src/dk/aau/cs/d703e20/codegen/ArduinoGenerator.java
+++ src/dk/aau/cs/d703e20/codegen/ArduinoGenerator.java
@@ -4,8 +4,9 @@ import dk.aau.cs.d703e20.ast.errorhandling.CompilerException;
 import dk.aau.cs.d703e20.ast.Enums;
 import dk.aau.cs.d703e20.ast.expressions.*;
 import dk.aau.cs.d703e20.ast.statements.*;
-import dk.aau.cs.d703e20.ast.structure.ProgramNode;
+import dk.aau.cs.d703e20.ast.structure.*;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedHashSet;
 
@@ -16,6 +17,7 @@ public class ArduinoGenerator {
 
     private static HashMap<String, VariableDeclarationNode> variables;
     private LinkedHashSet<String> usedVariables;
+    private ArrayList<FunctionDeclarationNode> Functions;
 
     public static HashMap<String, VariableDeclarationNode> getVariables() {
         return variables;
@@ -27,7 +29,9 @@ public class ArduinoGenerator {
         variables = new HashMap<>();
         usedVariables = new LinkedHashSet<>();
 
-        //TODO: ProgramNode program = visitProgram(ast);
+        Functions = new ArrayList<>();
+
+        ProgramNode program = visitProgram(ast);
 
         //TODO: generateSetup();
         //TODO: generateLoop();
@@ -35,6 +39,41 @@ public class ArduinoGenerator {
 
         return sb.toString();
     }
+
+
+    private ProgramNode visitProgram(ProgramNode programNode) {
+        SetupNode setupNode = visitSetup(programNode.getSetupNode());
+        LoopNode loopNode = visitLoop(programNode.getLoopNode());
+        ArrayList<FunctionDeclarationNode> functionDeclarationNodes = new ArrayList<>();
+
+        for (FunctionDeclarationNode functionDeclarationNode : programNode.getFunctionDeclarationNodes()) {
+            functionDeclarationNodes.add(visitFunctionDeclaration(functionDeclarationNode));
+        }
+
+        return new ProgramNode(setupNode, loopNode, functionDeclarationNodes);
+    }
+
+    //TODO visitBlock
+    private SetupNode visitSetup(SetupNode setupNode) {
+        return new SetupNode(visitBlock(setupNode.getBlockNode()));
+    }
+
+    //TODO visitBlock
+    private LoopNode visitLoop(LoopNode loopNode) {
+        return new LoopNode(visitBlock(loopNode.getBlockNode()));
+    }
+
+    private FunctionDeclarationNode visitFunctionDeclaration(FunctionDeclarationNode functionDeclarationNode) {
+        if (functionDeclarationNode.getDataType() != null) {
+            Functions.add(functionDeclarationNode);
+        }
+        else {
+            // Should have been inserted into main. Do nothing here TODO
+        }
+        return functionDeclarationNode;
+    }
+
+
 
 
 }
