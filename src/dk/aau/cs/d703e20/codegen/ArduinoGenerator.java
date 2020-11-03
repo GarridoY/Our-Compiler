6741724e70727771471757754d78f789d93e7b35
Rynke <37619846+Rynke@users.noreply.github.com>
2020-11-03 13:38:04 +0100

    initial codegen stuff

--- src/dk/aau/cs/d703e20/codegen/ArduinoGenerator.java
+++ src/dk/aau/cs/d703e20/codegen/ArduinoGenerator.java
@@ -4,7 +4,37 @@ import dk.aau.cs.d703e20.ast.errorhandling.CompilerException;
 import dk.aau.cs.d703e20.ast.Enums;
 import dk.aau.cs.d703e20.ast.expressions.*;
 import dk.aau.cs.d703e20.ast.statements.*;
+import dk.aau.cs.d703e20.ast.structure.ProgramNode;
+
+import java.util.HashMap;
+import java.util.LinkedHashSet;
 
 
 public class ArduinoGenerator {
+
+    private StringBuilder sb;
+
+    private static HashMap<String, VariableDeclarationNode> variables;
+    private LinkedHashSet<String> usedVariables;
+
+    public static HashMap<String, VariableDeclarationNode> getVariables() {
+        return variables;
+    }
+
+    public String GenerateArduino(ProgramNode ast) {
+        sb = new StringBuilder();
+
+        variables = new HashMap<>();
+        usedVariables = new LinkedHashSet<>();
+
+        //TODO: ProgramNode program = visitProgram(ast);
+
+        //TODO: generateSetup();
+        //TODO: generateLoop();
+        //TODO generateFunctions();
+
+        return sb.toString();
+    }
+
+
 }
