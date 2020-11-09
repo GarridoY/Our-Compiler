a000b99c0364e06c211eb69abd8cb77181a39dbe
Marcus <mrynkeby@hotmail.com>
2020-11-09 15:01:29 +0100

    More codegen visitors

--- src/dk/aau/cs/d703e20/codegen/ArduinoGenerator.java
+++ src/dk/aau/cs/d703e20/codegen/ArduinoGenerator.java
@@ -1,10 +1,7 @@
 package dk.aau.cs.d703e20.codegen;
 
 import dk.aau.cs.d703e20.ast.Enums;
-import dk.aau.cs.d703e20.ast.expressions.ArithExpressionNode;
-import dk.aau.cs.d703e20.ast.expressions.ArrayParamNode;
-import dk.aau.cs.d703e20.ast.expressions.BoolExpressionNode;
-import dk.aau.cs.d703e20.ast.expressions.ConditionalExpressionNode;
+import dk.aau.cs.d703e20.ast.expressions.*;
 import dk.aau.cs.d703e20.ast.statements.*;
 import dk.aau.cs.d703e20.ast.structure.*;
 import dk.aau.cs.d703e20.errorhandling.*;
@@ -83,7 +80,7 @@ public class ArduinoGenerator {
         else if (statementNode instanceof IfElseStatementNode)
             statementNodes.add(visitIfElseStatement((IfElseStatementNode)statementNode));
         else if (statementNode instanceof ReturnStatementNode)
-            statementNodes.add(visitReturnStatemnent((ReturnStatementNode)statementNode));
+            statementNodes.add(visitReturnStatement((ReturnStatementNode)statementNode));
         else if (statementNode instanceof FunctionCallNode)
             statementNodes.add(visitFunctionCall((FunctionCallNode)statementNode));
         else if (statementNode instanceof ForStatementNode)
@@ -116,24 +113,24 @@ public class ArduinoGenerator {
     }
 
     private IfElseStatementNode visitIfElseStatement(IfElseStatementNode ifElseStatementNode){
-        IfStatementNode ifStatementNode = visitIfStatementNode(ifElseStatementNode.getIfStatementNode());
+        IfStatementNode ifStatementNode = visitIfStatement(ifElseStatementNode.getIfStatementNode());
         ElseStatementNode elseStatementNode = null;
         ArrayList<ElseIfStatementNode> elseIfStatementNodes = new ArrayList<>();
 
         if (ifElseStatementNode.getElseIfStatementNodes() != null) {
             for (ElseIfStatementNode elseIfStatementNode : ifElseStatementNode.getElseIfStatementNodes()) {
-                elseIfStatementNodes.add(visitElseIfStatementNode(elseIfStatementNode));
+                elseIfStatementNodes.add(visitElseIfStatement(elseIfStatementNode));
             }
         }
 
         if (ifElseStatementNode.getElseStatement() != null) {
-            elseStatementNode = visitElseStatementNode(ifElseStatementNode.getElseStatement());
+            elseStatementNode = visitElseStatement(ifElseStatementNode.getElseStatement());
         }
 
         return new IfElseStatementNode(ifStatementNode, elseIfStatementNodes, elseStatementNode);
     }
 
-    private IfStatementNode visitIfStatementNode(IfStatementNode ifStatementNode) {
+    private IfStatementNode visitIfStatement(IfStatementNode ifStatementNode) {
         ConditionalExpressionNode conditionalExpressionNode = null;
         BlockNode blockNode;
 
@@ -148,6 +145,31 @@ public class ArduinoGenerator {
         else throw new CompilerException("Invalid if-statement", ifStatementNode.getCodePosition());
     }
 
+    private ElseIfStatementNode visitElseIfStatement(ElseIfStatementNode elseIfStatementNode){
+        ConditionalExpressionNode conditionalExpressionNode = null;
+        BlockNode blockNode;
+
+        if (elseIfStatementNode.getConditionalExpressionNode() != null) {
+            conditionalExpressionNode = visitConditionalExpression(elseIfStatementNode.getConditionalExpressionNode());
+        }
+
+        if (elseIfStatementNode.getBlockNode() != null) {
+            blockNode = visitBlock(elseIfStatementNode.getBlockNode());
+            return new ElseIfStatementNode(conditionalExpressionNode, blockNode);
+        }
+        else
+            throw new CompilerException("Invalid else-if-statement", elseIfStatementNode.getCodePosition());
+    }
+
+    private ElseStatementNode visitElseStatement(ElseStatementNode elseStatementNode) {
+        BlockNode blockNode;
+        if (elseStatementNode.getBlockNode() != null) {
+            blockNode = visitBlock(elseStatementNode.getBlockNode());
+            return new ElseStatementNode(blockNode);
+        }
+        else throw new CompilerException("Invalid else-statement", elseStatementNode.getCodePosition());
+    }
+
     private FunctionDeclarationNode visitFunctionDeclaration(FunctionDeclarationNode functionDeclarationNode) {
         if (functionDeclarationNode.getDataType() != null) {
             Functions.add(functionDeclarationNode);
@@ -156,6 +178,43 @@ public class ArduinoGenerator {
             throw new CompilerException("Function without data type.", functionDeclarationNode.getCodePosition());
         return functionDeclarationNode;
     }
+
+    private FunctionCallNode visitFunctionCall(FunctionCallNode functionCallNode) {
+        ArrayList<FunctionArgNode> functionArgNodes = new ArrayList<>();
+        String functionName = functionCallNode.getFunctionName();
+
+        if (functionCallNode.getFunctionArgNodes() != null) {
+            for (FunctionArgNode argNode : functionCallNode.getFunctionArgNodes()) {
+                functionArgNodes.add(visitArgument(argNode));  //TODO do this
+            }
+        }
+    
+        return new FunctionCallNode(functionName, functionArgNodes);
+    }
+
+    private ConditionalExpressionNode visitConditionalExpression(ConditionalExpressionNode conditionalExpressionNode){
+        BoolExpressionNode boolExpressionNode;
+        if (conditionalExpressionNode.getBoolExpressionNode() != null) {
+            boolExpressionNode = visitBoolExpression(conditionalExpressionNode.getBoolExpressionNode());
+            return new ConditionalExpressionNode(boolExpressionNode);
+        }
+        else throw new CompilerException("Invalid conditional-expression", conditionalExpressionNode.getCodePosition());
+    }
+
+    private BoolExpressionNode visitBoolExpression(BoolExpressionNode boolExpressionNode){
+        if (boolExpressionNode.getBoolExprOperandNodes() != null)
+            visitBoolOperation(boolExpressionNode.getBoolExpressionOperators()); //TODO it's a trap!
+        return  boolExpressionNode;
+    }
+
+    private ReturnStatementNode visitReturnStatement(ReturnStatementNode returnStatementNode){
+        if (returnStatementNode.getVariableName() != null) {
+            return returnStatementNode;
+        }
+        else throw new CompilerException("Invalid return-statement", returnStatementNode.getCodePosition());
+    }
+
+
 
 
 
