ad53a8f837f6556d41f2fb9507016960cea06d45
Simon Mathiasen <smath17@student.aau.dk>
2020-11-09 13:13:36 +0100

    fix Jdom
    (bye)

--- src/dk/aau/cs/d703e20/uppaal/ModelGen.java
+++ src/dk/aau/cs/d703e20/uppaal/ModelGen.java
@@ -1,26 +1,14 @@
 package dk.aau.cs.d703e20.uppaal;
 
-import dk.aau.cs.d703e20.ast.Enums;
-import dk.aau.cs.d703e20.ast.statements.VariableDeclarationNode;
-import org.jdom2.Attribute;
-import org.jdom2.DocType;
-import org.jdom2.Document;
-import org.jdom2.Element;
-import org.jdom2.output.Format;
-import org.jdom2.output.XMLOutputter;
-
-import java.io.IOException;
+import com.uppaal.model.core2.Edge;
+import com.uppaal.model.core2.Location;
+import com.uppaal.model.core2.Property;
+import com.uppaal.model.core2.Template;
+import dk.aau.cs.d703e20.ast.structure.ProgramNode;
 
 
 public class ModelGen {
-    // File handling
-    FileHandler fileHandler = new FileHandler();
 
-    // Root element
-    Element root = new Element("nta");
-    // Document type and header
-    DocType dtype = new DocType(root.getName(), "-//Uppaal Team//DTD Flat System 1.1//EN", "http://www.it.uu.se/research/group/darts/uppaal/flat-1_2.dtd");
-    Document doc = new Document(root, dtype);
 
     /* TODO:
         Each template is:
@@ -31,65 +19,95 @@ public class ModelGen {
         4. Queries
     */
 
-    private String visitVariableDecl(VariableDeclarationNode varDeclNode) {
-        // TODO: check UPPAAL array declaration, placement of brackets, frick, int a[]
-        Enums.DataType type = varDeclNode.getDataType();
-        // If declaration is array type
-        if (type == Enums.DataType.BOOL_ARRAY || type == Enums.DataType.DOUBLE_ARRAY || type == Enums.DataType.INT_ARRAY) {
-            if (varDeclNode.getAssignArrayNode() != null) {
+    /**
+     * Sets a label on a location.
+     * @param l the location on which the label is going to be attached
+     * @param kind a kind of the label
+     * @param value the label value (either boolean or String)
+     * @param x the x coordinate of the label
+     * @param y the y coordinate of the label
+     */
+    public static void setLabel(Location l, ModelDemo.LKind kind, Object value, int x, int y) {
+        l.setProperty(kind.name(), value);
+        Property p = l.getProperty(kind.name());
+        p.setProperty("x", x);
+        p.setProperty("y", y);
+    }
 
-            }
-            // else allocatedSize != null
-            else {
-                return type.toString() + " " + varDeclNode.getVariableName();
-            }
+    /**
+     * Adds a location to a template.
+     * @param t the template
+     * @param name a name for the new location
+     * @param x the x coordinate of the location
+     * @param y the y coordinate of the location
+     * @return the new location instance
+     */
+    public static Location addLocation(Template t, String name, String exprate,
+                                       int x, int y) {
+        Location l = t.createLocation();
+        t.insert(l, null);
+        l.setProperty("x", x);
+        l.setProperty("y", y);
+        if (name != null)
+            setLabel(l, ModelDemo.LKind.name, name, x, y - 28);
+        if (exprate != null)
+            setLabel(l, ModelDemo.LKind.exponentialrate, exprate, x, y - 28 - 12);
+        return l;
+    }
+
+    /**
+     * Sets a label on an edge.
+     * @param e the edge
+     * @param kind the kind of the label
+     * @param value the content of the label
+     * @param x the x coordinate of the label
+     * @param y the y coordinate of the label
+     */
+    public static void setLabel(Edge e, ModelDemo.EKind kind, String value, int x, int y) {
+        e.setProperty(kind.name(), value);
+        Property p = e.getProperty(kind.name());
+        p.setProperty("x", x);
+        p.setProperty("y", y);
+    }
+
+    /**
+     * Adds an edge to the template
+     * @param t the template where the edge belongs
+     * @param source the source location
+     * @param target the target location
+     * @param guard guard expression
+     * @param sync synchronization expression
+     * @param update update expression
+     * @return
+     */
+    public static Edge addEdge(Template t, Location source, Location target,
+                               String guard, String sync, String update) {
+        Edge e = t.createEdge();
+        t.insert(e, null);
+        e.setSource(source);
+        e.setTarget(target);
+        int x = (source.getX() + target.getX()) / 2;
+        int y = (source.getY() + target.getY()) / 2;
+        if (guard != null) {
+            setLabel(e, ModelDemo.EKind.guard, guard, x - 15, y - 28);
         }
-        return null;
-    }
-
-    void createXML() {
-        //first element
-        Element supercarElement = new Element("declaration");
-
-        supercarElement.addContent("chan sched2;\n");
-        supercarElement.addContent("chan sched1;\n");
-        supercarElement.addContent("clock x;");
-
-        //nested elements
-        Element carElement1 = new Element("carname");
-        carElement1.setAttribute(new Attribute("type", "formula one"));
-        carElement1.setAttribute(new Attribute("id", "id2"));
-        carElement1.setText("Ferrari 101");
-
-        Element carElement2 = new Element("carname");
-        carElement2.setAttribute(new Attribute("type", "sports"));
-        carElement2.setText("Ferrari 202");
-
-        supercarElement.addContent(carElement1);
-        supercarElement.addContent(carElement2);
-
-        doc.getRootElement().addContent(supercarElement);
-
-    }
-
-    void outputXml() {
-        try {
-            XMLOutputter xmlOutput = new XMLOutputter();
-
-            // display ml
-            xmlOutput.setFormat(Format.getPrettyFormat());
-            xmlOutput.output(doc, fileHandler.getWriter()); // FileHandler --> system.out for test print
-        } catch (IOException e) {
-            e.printStackTrace();
+        if (sync != null) {
+            setLabel(e, ModelDemo.EKind.synchronisation, sync, x - 15, y - 14);
         }
+        if (update != null) {
+            setLabel(e, ModelDemo.EKind.assignment, update, x - 15, y);
+        }
+        return e;
     }
 
-    public static void main(String[] args) {
-        VariableDeclarationNode variableDeclarationNode = new VariableDeclarationNode(Enums.DataType.INT_ARRAY, 5, "Hej123");
-        System.out.println(variableDeclarationNode.prettyPrint(0));
-        ModelGen modelGen = new ModelGen();
-        modelGen.createXML();
-        modelGen.outputXml();
+    // Visitors
+
+    void visitProgram(ProgramNode programNode) {
+        visitSetup();
+        //TODO: visitLoop();
+    }
+
+    void visitSetup() {
 
     }
 }
