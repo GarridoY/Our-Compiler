d1bc9dc71f836dde14dd1ccdc8dce65968bd245d
HTML_Earth <htmlearth@gmail.com>
2020-11-24 14:21:05 +0100

    Biggest commit of the day
    Wow!

--- src/dk/aau/cs/d703e20/codegen/arduino/code/Functions.java
+++ src/dk/aau/cs/d703e20/codegen/arduino/code/Functions.java
@@ -3,7 +3,7 @@ package dk.aau.cs.d703e20.codegen.arduino.code;
 public class Functions {
     public static String dWrite =
             "void dWrite(int pin, int value) {\n\t" +
-                "if (value == HIGH || value ==  LOW)\n\t\t" +
+                "if (value == HIGH || value == LOW)\n\t\t" +
                     "digitalWrite(pin, value);\n\t" +
                 "else\n\t\t" +
                     "digitalWrite(pin, value ? HIGH : LOW);\n" +
